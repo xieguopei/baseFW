d3088b91d7d26e119cde20fc2138e61d8caa1aea
xieguopei <xieguopei@163.com>
2019-02-27 18:57:12 +0800

    IOC接口

---
+++ common/framework/core/ioc/src/main/java/www/xieguopei/com/custom/BaseBeanFactoryPostProcessor.java
@@ -0,0 +1,6 @@
+package www.xieguopei.com.custom;
+
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+
+public interface BaseBeanFactoryPostProcessor extends BeanFactoryPostProcessor {
+}
